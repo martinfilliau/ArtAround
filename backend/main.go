55a8dbb49d2c0cc96a33320c45544a7857f85535
Martin Filliau <martin@filliau.com>
2018-08-07 22:10:34 +0200

    feat(backend): CORS middleware

--- backend/main.go
+++ backend/main.go
@@ -8,9 +8,27 @@ import (
 	"github.com/olivere/elastic"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	r := gin.Default()
 	ctx := context.Background()
+
+	r.Use(CORSMiddleware())
 
 	client, err := elastic.NewClient()
 	if err != nil {
@@ -34,5 +52,6 @@ func main() {
 			},
 		})
 	})
+
 	r.Run()
 }
